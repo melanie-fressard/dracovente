748e78aa044d560755b80b3e287e4a34245e2edf
Mélanie Fressard <fressardmelanie@gmail.com>
2020-12-21 22:08:32 +0100

    mention

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function verif () //affiche un element si la case est cochée
   {
-      var etat = document.getElementById('check').checked;
+      var etat2 = document.getElementById('check').checked;
        
-      if(!etat)
+      if(!etat2)
       {
           document.getElementById('2').className = 'off';
       }
